2db26644a7fe261b9bb3629105058b3545c49c7f
minseo <@example.com>
2026-05-19 14:40:56 +0900

    entity폴더 생성

--- src/main/java/com/example/CampusMarket/controller/IndexController.java
+++ src/main/java/com/example/CampusMarket/controller/IndexController.java
@@ -9,7 +9,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        // 🌟 'model' 가방에 "userName"이라는 이름으로 "민서" 데이터를 직접 실어서 화면으로 던집니다.
+
         model.addAttribute("userName", "민서");
 
         return "index"; // templates/index.mustache로 이동
