89b18dfb9b68884082c08dba55a50ff46d3e3d26
kimmay <kh2kyj@gmail.com>
2026-05-22 19:19:14 +0900

    details
    상품 페이지 디테일 레이아웃

--- src/main/java/com/example/CampusMarket/controller/IndexController.java
+++ src/main/java/com/example/CampusMarket/controller/IndexController.java
@@ -1,17 +1,43 @@
 package com.example.CampusMarket.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model; // 🌟 이거 추가되었는지 확인!
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    /* @GetMapping("/")
     public String index(Model model) {
 
         model.addAttribute("userName", "민서");
 
         return "index"; // templates/index.mustache로 이동
-    }
+    } */
+
+    @GetMapping("/")
+    public String index(
+        @RequestParam(required = false) String category,
+        @RequestParam(required = false) String status,
+        Model model
+        ) {
+            /* List<Product> products = productService.findByFilter(category, status);
+
+            model.addAttribute("products", products); */
+
+            model.addAttribute("userName", "강냉이");
+            model.addAttribute("category", category);
+            model.addAttribute("status", status);
+
+        return "index";
+        }
+    
+    @GetMapping("/product/{id}")
+    public String productDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("id", id);
+        
+        return "product/detail";
+        }
 }
